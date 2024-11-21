f64bae47b6e58bb044e4b44dcd96fb4007f8e5d4
kkorkemm <zhardemova2012@mail.ru>
2024-11-21 19:35:32 +0700

    task11

---
+++ WebApplication1/WebApplication1/Models/CalculatorViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class CalculatorViewModel
+{
+    public int Number1 { get; set; }
+    public int Number2 { get; set; }
+    public int Sum { get; set; }
+    public int Difference { get; set; }
+    public int Product { get; set; }
+    public double Quotient { get; set; }
+}
